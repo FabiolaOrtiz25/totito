b77c783d86c014b42bee5e2f6d08c109aa5d0b53
Fabyy.off <124743101+FabiolaOrtiz25@users.noreply.github.com>
2023-05-25 14:30:54 -0600

    Update jav.js

--- jav.js
+++ jav.js
@@ -1,7 +1,7 @@
 function mil(){ 
     document.getElementById('fab').src="circulo.jpg";
-    document.getElementById('fab').style.height="140px";
-    document.getElementById('fab').style.width="140px";
+    document.getElementById('fab').style.height="240px";
+    document.getElementById('fab').style.width="240px";
     document.getElementById('fab').style.display="none";
     
         // body...
@@ -9,15 +9,15 @@ function mil(){
     }
     function norte(){ 
     document.getElementById('yo').src="equis.jpg";
-    document.getElementById('yo').style.height="140px";
-    document.getElementById('yo').style.width="140px";
+    document.getElementById('yo').style.height="240px";
+    document.getElementById('yo').style.width="240px";
     document.getElementById('yo').style.display="none";
         // body...
     }
     function mac(){ 
     document.getElementById('lalo').src="circulo.jpg";
-    document.getElementById('lalo').style.height="140px";
-    document.getElementById('lalo').style.width="140px";
+    document.getElementById('lalo').style.height="240px";
+    document.getElementById('lalo').style.width="240px";
     document.getElementById('io').style.display="none";
     
         // body...
@@ -25,15 +25,15 @@ function mil(){
     }
     function sur(){ 
     document.getElementById('io').src="equis.jpg";
-    document.getElementById('io').style.height="140px";
-    document.getElementById('io').style.width="140px";
+    document.getElementById('io').style.height="240px";
+    document.getElementById('io').style.width="240px";
     document.getElementById('lalo').style.display="none";
         // body...
     }
     function miller(){ 
     document.getElementById('kiko').src="circulo.jpg";
-    document.getElementById('kiko').style.height="140px";
-    document.getElementById('kiko').style.width="140px";
+    document.getElementById('kiko').style.height="240px";
+    document.getElementById('kiko').style.width="240px";
     document.getElementById('lola').style.display="none";
     
         // body...
@@ -41,15 +41,15 @@ function mil(){
     }
     function este(){ 
     document.getElementById('lola').src="equis.jpg";
-    document.getElementById('lola').style.height="140px";
-    document.getElementById('lola').style.width="140px";
+    document.getElementById('lola').style.height="240px";
+    document.getElementById('lola').style.width="240px";
     document.getElementById('kiko').style.display="none";
         // body...
     }
     function luna(){ 
     document.getElementById('azul').src="circulo.jpg";
-    document.getElementById('azul').style.height="140px";
-    document.getElementById('azul').style.width="140px";
+    document.getElementById('azul').style.height="240px";
+    document.getElementById('azul').style.width="240px";
     document.getElementById('rojo').style.display="none";
     
         // body...
@@ -57,15 +57,15 @@ function mil(){
     }
     function mio(){ 
     document.getElementById('rojo').src="equis.jpg";
-    document.getElementById('rojo').style.height="140px";
+    document.getElementById('rojo').style.height="240px";
     document.getElementById('rojo').style.width="140px";
     document.getElementById('azul').style.display="none";
         // body...
     }
     function estrella(){ 
     document.getElementById('amar').src="circulo.jpg";
-    document.getElementById('amar').style.height="140px";
-    document.getElementById('amar').style.width="140px";
+    document.getElementById('amar').style.height="240px";
+    document.getElementById('amar').style.width="240px";
     document.getElementById('mar').style.display="none";
     
         // body...
@@ -73,15 +73,15 @@ function mil(){
     }
     function eli(){ 
     document.getElementById('mar').src="equis.jpg";
-    document.getElementById('mar').style.height="140px";
-    document.getElementById('mar').style.width="140px";
+    document.getElementById('mar').style.height="240px";
+    document.getElementById('mar').style.width="240px";
     document.getElementById('amar').style.display="none";
         // body...
     }
     function coco(){ 
     document.getElementById('lol').src="circulo.jpg";
-    document.getElementById('lol').style.height="140px";
-    document.getElementById('lol').style.width="140px";
+    document.getElementById('lol').style.height="240px";
+    document.getElementById('lol').style.width="240px";
     document.getElementById('dose').style.display="none";
     
         // body...
@@ -89,15 +89,15 @@ function mil(){
     }
     function mimi(){ 
     document.getElementById('dolge').src="equis.jpg";
-    document.getElementById('dolge').style.height="140px";
-    document.getElementById('dolge').style.width="140px";
+    document.getElementById('dolge').style.height="240px";
+    document.getElementById('dolge').style.width="240px";
     document.getElementById('lol').style.display="none";
         // body...
     }
     function ela(){ 
     document.getElementById('alp').src="circulo.jpg";
-    document.getElementById('alp').style.height="140px";
-    document.getElementById('alp').style.width="140px";
+    document.getElementById('alp').style.height="240px";
+    document.getElementById('alp').style.width="240px";
     document.getElementById('sor').style.display="none";
     
         // body...
@@ -105,15 +105,15 @@ function mil(){
     }
     function a(){ 
     document.getElementById('sor').src="equis.jpg";
-    document.getElementById('sor').style.height="140px";
-    document.getElementById('sor').style.width="140px";
+    document.getElementById('sor').style.height="240px";
+    document.getElementById('sor').style.width="240px";
     document.getElementById('alp').style.display="none";
         // body...
     }
     function sejo(){ 
     document.getElementById('taya').src="circulo.jpg";
-    document.getElementById('taya').style.height="140px";
-    document.getElementById('taya').style.width="140px";
+    document.getElementById('taya').style.height="240px";
+    document.getElementById('taya').style.width="240px";
     document.getElementById('tayo').style.display="none";
     
         // body...
@@ -121,15 +121,15 @@ function mil(){
     }
     function l(){ 
     document.getElementById('tayo').src="circulo.jpg";
-    document.getElementById('tayo').style.height="140px";
-    document.getElementById('tayo').style.width="140px";
+    document.getElementById('tayo').style.height="240px";
+    document.getElementById('tayo').style.width="240px";
     document.getElementById('tayo').style.display="none";
         // body...
     }
     function aca(){ 
     document.getElementById('yeah').src="ciculo.jpg";
-    document.getElementById('yeah').style.height="140px";
-    document.getElementById('yeah').style.width="140px";
+    document.getElementById('yeah').style.height="240px";
+    document.getElementById('yeah').style.width="240px";
     document.getElementById('yep').style.display="none";
     
         // body...
@@ -137,8 +137,8 @@ function mil(){
     }
     function e(){ 
     document.getElementById('yep').src="equis.jpg";
-    document.getElementById('yep').style.height="140px";
-    document.getElementById('yep').style.width="140px";
+    document.getElementById('yep').style.height="240px";
+    document.getElementById('yep').style.width="240px";
     document.getElementById('yeah').style.display="none";
         // body...
     }
